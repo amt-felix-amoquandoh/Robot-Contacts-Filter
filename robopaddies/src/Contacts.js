7aa1c295df12174391820c1d4c51dae86dc5f143
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2023-04-12 20:42:08 +0000

    index updated

--- robopaddies/src/Contacts.js
+++ robopaddies/src/Contacts.js
@@ -4,7 +4,7 @@ import "./contacts.css"
 class Contacts extends Component{
     render(){
         return (
-        <div>
+        <div className="f1 tc">
             <h1>Hello World</h1>
             <p>IM Baaaaaaaackk!!!!!</p>
         </div>
--- robopaddies/src/index.js
+++ robopaddies/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'tachyons';
 import Contacts from './Contacts';
 import reportWebVitals from './reportWebVitals';
 
