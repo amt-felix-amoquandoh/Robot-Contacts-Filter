08eeed408e5792a84cf846edfa8d4584b2509d9b
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2023-04-12 21:32:53 +0000

    Create contacts.js

---
+++ robopaddies/src/contacts.js
@@ -0,0 +1,60 @@
+export const contacts = [
+    {
+        id: 1,
+        name: "Asher Amoquandoh",
+        username: "Yoofi",
+        email: "youngknight@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Asher Amoquandoh",
+        username: "Yoofi",
+        email: "youngknight@gmail.com"
+    },
+    {
+        id: 3,
+        name: "Asher Amoquandoh",
+        username: "Yoofi",
+        email: "youngknight@gmail.com"
+    },
+    {
+        id: 4,
+        name: "Asher Amoquandoh",
+        username: "Yoofi",
+        email: "youngknight@gmail.com"
+    }, {
+        id: 5,
+        name: "Asher Amoquandoh",
+        username: "Yoofi",
+        email: "youngknight@gmail.com"
+    },
+    {
+        id: 6,
+        name: "Asher Amoquandoh",
+        username: "Yoofi",
+        email: "youngknight@gmail.com"
+    },
+    {
+        id: 7,
+        name: "Asher Amoquandoh",
+        username: "Yoofi",
+        email: "youngknight@gmail.com"
+    },
+    {
+        id: 8,
+        name: "Asher Amoquandoh",
+        username: "Yoofi",
+        email: "youngknight@gmail.com"
+    },
+    {
+        id: 9,
+        name: "Asher Amoquandoh",
+        username: "Yoofi",
+        email: "youngknight@gmail.com"
+    }, {
+        id: 10,
+        name: "Asher Amoquandoh",
+        username: "Yoofi",
+        email: "youngknight@gmail.com"
+    }
+]
