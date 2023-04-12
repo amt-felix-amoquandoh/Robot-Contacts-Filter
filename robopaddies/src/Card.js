e609b40b9d15c418dbb5bd471ff2fb91a9bf0ba4
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2023-04-12 21:00:08 +0000

    Update Card.js

--- robopaddies/src/Card.js
+++ robopaddies/src/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = () => {
     return(
-        <div>
+        <div className="bg-light-green dib br3 pa3 ma2 grow">
             <img src="https://robohash.org/ddffsss?150x150" alt="robot"/>
             <div>
                 <h2>Asher Yoofi</h2>
