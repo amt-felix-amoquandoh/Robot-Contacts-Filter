e230d1c35926418707bedddc1d0c5c3a40644e5e
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2023-04-12 23:12:12 +0000

    contacts updated

--- robopaddies/src/CardList.js
+++ robopaddies/src/CardList.js
@@ -3,13 +3,19 @@ import React from "react";
 import Card from "./Card"
 
 const CardList = ({contacts}) =>{
-   const contactCard = contacts.map((user, i) => {
-       return  <Card id={contacts[i].id} name={contacts[i].name} email={contacts[i].email} />
-   })
-   return(
-    <div>
-      {contactCard}
-    </div>
+   return (
+     <div>
+       {contacts.map((user, i) => {
+         return (
+           <Card
+             key={i}
+             id={contacts[i].id}
+             name={contacts[i].name}
+             email={contacts[i].email}
+           />
+         );
+       })}
+     </div>
    );
 }
 
