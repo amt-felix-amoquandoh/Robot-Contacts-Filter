873e3d0891129697ebe08a9298d1230ab1aa17a8
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2023-04-12 20:03:51 +0000

    robo update

---
+++ robopaddies/src/Contacts.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Contacts extends Component{
+    render(){
+        return (
+        <div>
+            <h1>Hello World</h1>
+            <p>IM Baaaaaaaackk!!!!!</p>
+        </div>
+        )
+    }
+}
+
+export default Contacts;
