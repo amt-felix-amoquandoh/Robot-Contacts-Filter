57451b9ab0d41dc8c4629c67f73e5f2f6c05108d
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2023-04-12 23:26:59 +0000

    Update App.js

--- robopaddies/src/App.js
+++ robopaddies/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { contacts } from "./contacts";
 import CardList from "./CardList";
+import SearchArea from "./Search"
 
 
 const App = () => {
     return(
         <div>
             <h1 className="tc">My Robot Contacts</h1>
+            <SearchArea/>
             <CardList contacts={contacts}/>
         </div>
     )
